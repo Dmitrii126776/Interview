1f11ec04c665622bb3e0599c356c83f7b1a6ca5f
Dmitrii126776 <dukek67@gmail.com>
2023-02-28 18:56:56 -0500

    KanbanBoard.js

---
+++ allstar/getNextNumber.js
@@ -0,0 +1,11 @@
+function getNextNumber() {
+    if (!getNextNumber.current) {
+        getNextNumber.current = 100;
+    }
+    const nextNumber = getNextNumber.current;
+    getNextNumber.current++;
+    return nextNumber;
+}
+
+console.log(getNextNumber() + 1); // 101
+
