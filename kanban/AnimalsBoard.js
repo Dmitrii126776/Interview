e57ae7b603bbda2058d56a480ea9721c966bbfa5
Dmitrii126776 <dukek67@gmail.com>
2023-03-09 22:34:28 -0500

    KanbanBoard.js

---
+++ kanban/AnimalsBoard.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+
+const AnimalsBoard = (props) => {
+    const {animals} = props
+    const [hoveredCard, setHoveredCard] = useState(null);
+
+    return (
+        <div className="container-fluid">
+            <div>
+                <h1>Board of Animals</h1>
+            </div>
+            <div className="row">
+                {animals.map((el, index) => (
+                    <div key={el._id} className="col-md-4 col-auto mb-3">
+                        <div className="card p-3" style={{width: "100%", cursor: "pointer"}}
+                             onMouseEnter={() => setHoveredCard(index)}
+                             onMouseLeave={() => setHoveredCard(null)}
+                        >
+                            <img src={el.mainPhoto} className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title">{el.name}</h5>
+                                <h6 className="card-text">{el.age}{', '}{el.sex} </h6>
+                                {hoveredCard === index && <div>click to view more...</div>}
+                            </div>
+                        </div>
+                        {((index + 1) % 3 === 0 || index === animals.length - 1) && <div className="w-100"></div>}
+                    </div>
+                ))}
+            </div>
+        </div>
+
+    );
+};
+
+export default AnimalsBoard;
+// <a href="https://ibb.co/jZkKVGt"><img src="https://i.ibb.co/5xhDYvZ/IMG-1432.jpg" alt="IMG-1432" border="0"></a>
